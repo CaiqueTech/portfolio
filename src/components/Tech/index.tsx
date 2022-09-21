12c03bcef522bd2f5f67eb6001092e3e23dcd617
Caique <dev.caiquemorales@gmail.com>
2022-09-21 10:47:17 -0300

    :construction: feat: Tech section

---
+++ src/components/Tech/index.tsx
@@ -0,0 +1,13 @@
+import { TechContainer, TechItem, TechS } from "./style"
+
+export function Tech = () => {
+  return(
+    <TechS>
+      <TechContainer>
+        <TechItem>
+
+        </TechItem>
+      </TechContainer>
+    </TechS>
+  )
+}
---
+++ src/components/Tech/style.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const TechS = styled.section``;
+
+export const TechContainer = styled.div``;
+
+export const TechItem = styled.div``;
